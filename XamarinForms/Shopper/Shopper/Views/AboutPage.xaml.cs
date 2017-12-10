d71a0bf59caf3dd6e6ffad9e279f20be1122a67e
Vijay Vepakomma <vijay.vepakomma@philips.com>
2017-12-10 09:20:35 -0500

    Add default xamarin forms app and made sure it runs on iPhone

---
+++ XamarinForms/Shopper/Shopper/Views/AboutPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace Shopper
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
